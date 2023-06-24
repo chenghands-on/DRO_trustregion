191ed10dd8096071d454413535cb74fa562e993b
lichenxi <lichenxi7733@163.com>
2023-06-24 14:28:44 +0800

    update to pydrsom-mode-correct

--- DRO_trustregion-correct/hello.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Using [{device}] for the work')
--- DRO_trustregion-correct/my_model.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-import torch.nn as nn
-from torchvision.models import resnet18
-
-model=resnet18(pretrained=False)
-fc_features=model.fc.in_features
-model.fc=nn.Linear(fc_features,1)
-print(model)
